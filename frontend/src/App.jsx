0190ba88b56d49e4a24b4a7f38265a68a76f41d0
eduardogarcia2 <eduardogarcia@advantedigital.com>
2024-02-27 23:52:10 -0600

    Added logo animated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
 import { Box, Typography } from "@mui/material";
 import Home from "./pages/Home";
+import UTD from "./assets/utd_logo.png";
+import { gsap } from 'gsap';
+import { useEffect, useRef } from "react";
 
 const App = () => {
+  const logoRef = useRef(null); // Referencia para el elemento del logo
+
+  const handleMouseEnter = () => {
+    gsap.to(logoRef.current, {
+      rotation:  360,
+      duration:  3, 
+      repeat: -1,
+      ease: 'none'
+    });
+  };
+
+  // Función para manejar el evento onMouseLeave
+  const handleMouseLeave = () => {
+    gsap.killTweensOf(logoRef.current); // Detiene todas las animaciones del logo
+  };
+
   return (
     <Box>
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 8 }}>
-        <Typography variant="h5">
+      <Box sx={{ display: "flex", justifyContent: "center", mt:  8, flexDirection: "column" }}>
+        <Typography variant="h5" sx={{ textAlign: "center", mb: "10px" }}>
           CRUD Curriculums
         </Typography>
+        <Box sx={{ textAlign: "center", mb: "10px" }}>
+          <img
+            ref={logoRef} // Asignar la referencia al elemento
+            className='logo'
+            src={UTD}
+            alt="Logo UTD"
+            style={{ width: "10rem" }}
+            onMouseEnter={handleMouseEnter} // Evento onMouseEnter
+            onMouseLeave={handleMouseLeave} // Evento onMouseLeave
+          />
+        </Box>
       </Box>
       <Home />
     </Box>
